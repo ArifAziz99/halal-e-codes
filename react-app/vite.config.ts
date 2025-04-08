549ae70922a5c914a64481ee9f1c674899752594
Mohammad Arif Aziz <md.arifaziz24599@gmail.com>
2025-04-08 09:32:28 +0530

    fix

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '/react-app',},
   plugins: [
     react(),
     tailwindcss(),
